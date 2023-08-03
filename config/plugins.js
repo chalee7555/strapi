8523ed8be335aea580a5d62ec047118e0c752dcb
arthit sokhampha <arthit.scs57@ubru.ac.th>
2023-08-03 11:23:59 +0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
       config: {
         deployHook:"https://api.vercel.com/v1/integrations/deploy/prj_3NkaFIRBqyxDiz4RhA0rnH1E3H0b/6HesBJ8AyB",
         apiToken: "7Ao0xKRJ6FoogA3Qk70NSfUa",
-        appFilter: "strapi-jet.vercel.app",
+        appFilter: "strapi",
        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
       },
     },
